41e0f7d1da49f954570f64408dbbb8ab9fc52a79
Facundo Ortiz <fortiz@matechstudios.com>
2022-05-25 09:17:07 -0300

    quick misc scripts

---
+++ misc-Get-Mailbox-pre-test.ps1
@@ -0,0 +1,36 @@
+Function Connect_Exo
+{
+ #Check for EXO v2 module inatallation
+ $Module = Get-Module ExchangeOnlineManagement -ListAvailable
+ if($Module.count -eq 0) 
+ { 
+  Write-Host Exchange Online PowerShell V2 module is not available  -ForegroundColor yellow  
+  $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No 
+  if($Confirm -match "[yY]") 
+  { 
+   Write-host "Installing Exchange Online PowerShell module"
+   Install-Module ExchangeOnlineManagement -Repository PSGallery -AllowClobber -Force
+   Import-Module ExchangeOnlineManagement
+  } 
+  else 
+  { 
+   Write-Host EXO V2 module is required to connect Exchange Online.Please install module using Install-Module ExchangeOnlineManagement cmdlet. 
+   Exit
+  }
+ } 
+ Write-Host Connecting to Exchange Online...
+ #Storing credential in script for scheduling purpose/ Passing credential as parameter - Authentication using non-MFA account
+ if(($UserName -ne "") -and ($Password -ne ""))
+ {
+  $SecuredPassword = ConvertTo-SecureString -AsPlainText $Password -Force
+  $Credential  = New-Object System.Management.Automation.PSCredential $UserName,$SecuredPassword
+  Connect-ExchangeOnline -Credential $Credential
+ }
+ else
+ {
+  Connect-ExchangeOnline
+ }
+}
+Connect_Exo
+
+Get-Mailbox fortiz | FL PrimarySmtpAddress,EmailAddresses,WindowsEmailAddress
---
+++ misc-remove-alias.ps1
@@ -0,0 +1,3 @@
+# To remove a single alias of a Mailbox run the following command
+
+Set-Mailbox MAILBOX –EmailAddresses @{remove=’alias@matechstudios.com’}
